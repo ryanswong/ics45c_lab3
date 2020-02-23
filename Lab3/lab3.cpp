df64206c72a91b3077a98d0e6f91785712e3368d
Ryan Wong <rswong2@uci.edu>
2020-02-22 17:16:40 -0800

    fixed sort, added overloaded << operator, finshed rentMovie(needs testing)

--- Lab3/lab3.cpp
+++ Lab3/lab3.cpp
@@ -5,9 +5,10 @@ int main()
 	//contruct MovieManager and calls run
 	Renter x;
 	Renter y(123, "ryan", "wong");
-	Renter z(456, "jinsoo", "song");
-	Renter z2(124, "jinsoo", "song");
-	Renter z3(125, "jinso", "song");
+	Renter z(456, "jin-woo", "sung");
+	Renter z2(124, "jin-woo", "sung");
+	Renter z3(125, "jin", "sung");
+	Renter z4(2468, "tristin", "bui");
 
 	//cout << y.getRenterId() << endl;
 	//cout << y.getRenterFn() << endl;
@@ -17,11 +18,15 @@ int main()
 	//cout << z.getRenterLn() << endl;
 
 	Movie m_obj;
+	m_obj.setMovieCode("PSTE");
+	m_obj.setMovieName("Parasite");
+	m_obj.setNumCopies(10);
 	m_obj.rentMovie(y);
 	m_obj.rentMovie(z);
 	m_obj.rentMovie(z2);
 	m_obj.rentMovie(z3);
 	m_obj.show_array();
+	cout << m_obj << endl;
 
 
 
